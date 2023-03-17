bc3f4198851471a0a41a91f33bddae56a466b010
TrueZeorOne <blaabjerg.anton@gmail.com>
2023-03-17 13:54:59 +0100

    Corners and sides

--- HexIntersteller/Assets/Scripts/PointsAndSides.cs
+++ HexIntersteller/Assets/Scripts/PointsAndSides.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PointsAndSides : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject cityPoint;
+    [SerializeField] private GameObject tileMap;
+    private void Start()
     {
         
-    }
+        List<Vector3> points = new List<Vector3>()
+        {
+            new Vector3(0,0,0.5f),
+            new Vector3(0,0,-0.5f),
+            new Vector3(0.4329883f,0,0.25f),
+            new Vector3(0.4329883f,0,-0.25f),
+            new Vector3(-0.4329883f,0,0.25f),
+            new Vector3(-0.4329883f,0,-0.25f)
 
-    // Update is called once per frame
-    void Update()
+        };
+        AddPoints(points);
+    }
+    public void AddPoints(List<Vector3> points)
     {
-        
+        bool samePlace = false;
+        for (int i = 0; i < points.Count; i++)
+        {
+            for (int j = 0; j < transform.childCount; j++)
+            {
+                if(transform.GetChild(j).transform.position == transform.position + points[i])
+                {
+                    samePlace = true;
+                }
+            }
+            if(!samePlace)
+                Instantiate(cityPoint, transform.position + points[i], transform.rotation, transform);
+        }
     }
 }
