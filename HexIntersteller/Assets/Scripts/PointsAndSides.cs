f44042455389a6fd6bb04c7554b0ae48198ea4da
TrueZeorOne <blaabjerg.anton@gmail.com>
2023-03-17 15:26:02 +0100

    Added lanes

--- HexIntersteller/Assets/Scripts/PointsAndSides.cs
+++ HexIntersteller/Assets/Scripts/PointsAndSides.cs
@@ -5,34 +5,38 @@ using UnityEngine;
 public class PointsAndSides : MonoBehaviour
 {
     [SerializeField] private GameObject cityPoint;
-    [SerializeField] private GameObject tileMap;
+    [SerializeField] private GameObject lanePoint;
+    [SerializeField] private GameObject pointParent;
+    [SerializeField] private GameObject sideParent;
     List<Vector3> points = new List<Vector3>()
-        {
-            new Vector3(0,0,0.5f),
-            new Vector3(0,0,-0.5f),
-            new Vector3(0.4329883f,0,0.25f),
-            new Vector3(0.4329883f,0,-0.25f),
-            new Vector3(-0.4329883f,0,0.25f),
-            new Vector3(-0.4329883f,0,-0.25f)
+    {
+        new Vector3(0,0,0.5f),
+        new Vector3(0,0,-0.5f),
+        new Vector3(0.4329883f,0,0.25f),
+        new Vector3(0.4329883f,0,-0.25f),
+        new Vector3(-0.4329883f,0,0.25f),
+        new Vector3(-0.4329883f,0,-0.25f)
 
-        };
+    };
+    List<Vector3> sides = new List<Vector3>()
+    {
+        new Vector3(0.2164941f,0,0.375f),
+        new Vector3(0.2164941f,0,-0.375f),
+        new Vector3(-0.2164941f,0,0.375f),
+        new Vector3(-0.2164941f,0,-0.375f),
+        new Vector3(0.4329882f,0,0),
+        new Vector3(-0.4329882f,0,0)
+
+    };
     private void Start()
     {
-        for (int i = 0; i < tileMap.transform.childCount; i++)
-        {
-            AddPoints(tileMap.transform.GetChild(i).gameObject);
-        }
         for (int i = 0; i < transform.childCount; i++)
         {
-            Transform childI = transform.GetChild(i);
-            for (int j = i+1; j < transform.childCount; j++)
-            {
-                Transform childJ = transform.GetChild(j);
-                if (childI.position == childJ.position)
-                    Destroy(childJ.gameObject);
-
-            }
+            AddPoints(transform.GetChild(i).gameObject);
+            Addsides(transform.GetChild(i).gameObject);
         }
+        CheckDoubble(pointParent);
+        CheckDoubble(sideParent);
     }
     public void AddPoints(GameObject child)
     {
@@ -46,7 +50,28 @@ public class PointsAndSides : MonoBehaviour
                     samePlace = true;
                 }
             }*/
-            Instantiate(cityPoint, child.transform.position + points[i], transform.rotation, transform);
+            Instantiate(cityPoint, child.transform.position + points[i], pointParent.transform.rotation, pointParent.transform);
+        }
+    }
+    public void Addsides(GameObject child)
+    {
+        for (int i = 0; i < sides.Count; i++)
+        {
+            Instantiate(lanePoint, child.transform.position + sides[i], sideParent.transform.rotation, sideParent.transform);
+        }
+    }
+    public void CheckDoubble(GameObject Check)
+    {
+        for (int i = 0; i < Check.transform.childCount; i++)
+        {
+            Transform childI = Check.transform.GetChild(i);
+            for (int j = i + 1; j < Check.transform.childCount; j++)
+            {
+                Transform childJ = Check.transform.GetChild(j);
+                if (childI.position == childJ.position)
+                    Destroy(childJ.gameObject);
+
+            }
         }
     }
 }
