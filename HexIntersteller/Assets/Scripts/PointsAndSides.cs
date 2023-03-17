d5e8db7013937576bb89b5037d2032db5f6de36b
TrueZeorOne <blaabjerg.anton@gmail.com>
2023-03-17 14:46:47 +0100

    Points not Sides

--- HexIntersteller/Assets/Scripts/PointsAndSides.cs
+++ HexIntersteller/Assets/Scripts/PointsAndSides.cs
@@ -6,10 +6,7 @@ public class PointsAndSides : MonoBehaviour
 {
     [SerializeField] private GameObject cityPoint;
     [SerializeField] private GameObject tileMap;
-    private void Start()
-    {
-        
-        List<Vector3> points = new List<Vector3>()
+    List<Vector3> points = new List<Vector3>()
         {
             new Vector3(0,0,0.5f),
             new Vector3(0,0,-0.5f),
@@ -19,22 +16,37 @@ public class PointsAndSides : MonoBehaviour
             new Vector3(-0.4329883f,0,-0.25f)
 
         };
-        AddPoints(points);
-    }
-    public void AddPoints(List<Vector3> points)
+    private void Start()
     {
-        bool samePlace = false;
+        for (int i = 0; i < tileMap.transform.childCount; i++)
+        {
+            AddPoints(tileMap.transform.GetChild(i).gameObject);
+        }
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform childI = transform.GetChild(i);
+            for (int j = i+1; j < transform.childCount; j++)
+            {
+                Transform childJ = transform.GetChild(j);
+                if (childI.position == childJ.position)
+                    Destroy(childJ.gameObject);
+
+            }
+        }
+    }
+    public void AddPoints(GameObject child)
+    {
+        //bool samePlace = false;
         for (int i = 0; i < points.Count; i++)
         {
-            for (int j = 0; j < transform.childCount; j++)
+            /*for (int j = 0; j < transform.childCount; j++)
             {
-                if(transform.GetChild(j).transform.position == transform.position + points[i])
+                if(transform.GetChild(j).transform.position == child.transform.position + points[i])
                 {
                     samePlace = true;
                 }
-            }
-            if(!samePlace)
-                Instantiate(cityPoint, transform.position + points[i], transform.rotation, transform);
+            }*/
+            Instantiate(cityPoint, child.transform.position + points[i], transform.rotation, transform);
         }
     }
 }
