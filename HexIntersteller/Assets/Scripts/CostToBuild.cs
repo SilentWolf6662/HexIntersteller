7e4713519b4ef8e4afb9a216f7c84e3661afe2e7
TrueZeorOne <blaabjerg.anton@gmail.com>
2023-03-30 11:28:13 +0200

    Update CostToBuild.cs

--- HexIntersteller/Assets/Scripts/CostToBuild.cs
+++ HexIntersteller/Assets/Scripts/CostToBuild.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HexInterstellar.Resources
 {
     public class CostToBuild : MonoBehaviour
     {
+        private Button button;
         [SerializeField] private int[] price;
         [SerializeField] private Materials[] mat;
         [SerializeField] private Resources resources;
@@ -14,8 +16,14 @@ namespace HexInterstellar.Resources
         {
             for (int i = 0; i < price.Length; i++)
             {
-                
+                int amount = resources.GetAmount(mat[i].ToString());
+                if (amount < price[i])
+                {
+                    button.interactable = false;
+                    return;
+                }
             }
+            button.interactable = true;
         }
     }
 
