4953129eed15db236e87ba9f1bd875f39a7932f4
SilentWolf <ninjagocola321@gmail.com>
2023-04-11 11:17:37 +0200

    Camera Movement Reworked V1

--- HexIntersteller/Assets/Scripts/CameraController.cs
+++ HexIntersteller/Assets/Scripts/CameraController.cs
@@ -6,58 +6,31 @@ namespace HexInterstellar
 {
 	public class CameraController : CinemachineInputProvider
 	{
-		[SerializeField] private GameObject moveTarget;
-		[SerializeField] private Rigidbody moveTargetRigidbody;
-		[SerializeField] private Transform cinemachineVCam;
-		[SerializeField] private InputActionReference activateOrbitControl, moveControl;
-		[SerializeField, Range(1, 10)] private float speedMultiplier = 2;
-		private InputAction activateOrbitAction, action, moveAction;
-		private bool isActivated;
-		private Vector2 moveInput;
-		private Vector3 forward, right, fowardRelative, rightRelative, moveDirRelative, velocity;
-		private float look;
+		[SerializeField] private CinemachineCameraOffset camOffset;
+		[SerializeField] private float sensitivity = 10;
+		[SerializeField] private InputActionReference activateOrbitInput, zoomInput;
+		private InputAction action;
+		private float look, cameraDistance;
+		private Vector2 zoomInputValue;
+		private bool iscomponentBaseNull;
 		private void Start()
 		{
-			activateOrbitAction = activateOrbitControl.action;
-			activateOrbitAction.Enable();
-			moveAction = moveControl.action;
-			moveAction.Enable();
+			activateOrbitInput.action.Enable();
+			zoomInput.action.Enable();
 		}
 		private void Update()
 		{
-			moveInput = moveAction.ReadValue<Vector2>();
-			forward = cinemachineVCam.forward;
-			right = cinemachineVCam.right;
-			fowardRelative = moveInput.y * forward;
-			rightRelative = moveInput.x * right;
-			moveDirRelative = fowardRelative + rightRelative;
-			moveTarget.transform.Translate(moveDirRelative * (Time.deltaTime * speedMultiplier), Space.World);
+			zoomInputValue = zoomInput.action.ReadValue<Vector2>();
+			camOffset.m_Offset.z += zoomInputValue.y * (sensitivity * Time.deltaTime);
 		}
 		// ReSharper disable Unity.PerformanceAnalysis
 		public override float GetAxisValue(int axis)
 		{
 			if (!enabled) return 0;
-			if (activateOrbitAction.ReadValue<float>() != 1) return 0;
+			if (activateOrbitInput.action.ReadValue<float>() != 1) return 0;
 			action = ResolveForPlayer(axis, axis == 2 ? ZAxis : XYAxis);
 			if (action == null) return 0;
-			// ReSharper disable once ConvertSwitchStatementToSwitchExpression
-			switch (axis)
-			{
-				case 0: 
-					look = action.ReadValue<Vector2>().x; 
-					break;
-				case 1: 
-					look = action.ReadValue<Vector2>().y; 
-					break;
-				case 2: 
-					look = action.ReadValue<float>(); 
-					break;
-				default: 
-					look = 0; 
-					break;
-			}
-			moveTarget.transform.LookAt(-cinemachineVCam.position);
-			return look;
+			return axis switch { 0 => action.ReadValue<Vector2>().x, 1 => action.ReadValue<Vector2>().y, 2 => action.ReadValue<float>(), _ => 0 };
 		}
 	}
 }
