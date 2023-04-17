2d6b3dae94cd4da6a8083999747c78f96fbaf3c8
SilentWolf <ninjagocola321@gmail.com>
2023-04-17 11:27:41 +0200

    Update CameraController.cs
    Added commentary and clean up

--- HexIntersteller/Assets/Scripts/Movement/CameraController.cs
+++ HexIntersteller/Assets/Scripts/Movement/CameraController.cs
@@ -10,24 +10,33 @@ namespace HexInterstellar.Movement
 		[SerializeField] private float sensitivity = 10;
 		[SerializeField] private InputActionReference activateOrbitInput, zoomInput;
 		private InputAction action;
-		private float look, cameraDistance;
+		private float cameraDistance;
 		private Vector2 zoomInputValue;
-		private bool iscomponentBaseNull;
 		private void Start()
 		{
+			// Activates the scroll wheel action input and right mouse button action input
 			activateOrbitInput.action.Enable();
 			zoomInput.action.Enable();
 		}
 		private void Update()
 		{
+			// Reads mouse wheel values in a Vector2
 			zoomInputValue = zoomInput.action.ReadValue<Vector2>();
+
+			// Takes z offset and adding mouse wheel y multiplied with sensitivity
+			// that multiplies with Time.deltaTime to make the same feeling of smooth zoom
 			camOffset.m_Offset.z += zoomInputValue.y * (sensitivity * Time.deltaTime);
 		}
 		// ReSharper disable Unity.PerformanceAnalysis
 		public override float GetAxisValue(int axis)
-		{                                            
+		{             
+			// Checks if input is enabled                               
 			if (!enabled) return 0;
+
+			// Checks if mouse right btn is held down
 			if (activateOrbitInput.action.ReadValue<float>() != 1) return 0;
+
+			// Resovles the movement based on axises
 			action = ResolveForPlayer(axis, axis == 2 ? ZAxis : XYAxis);
 			if (action == null) return 0;
 			return axis switch { 0 => action.ReadValue<Vector2>().x, 1 => action.ReadValue<Vector2>().y, 2 => action.ReadValue<float>(), _ => 0 };
