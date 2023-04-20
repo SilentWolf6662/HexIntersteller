5a56737932a80fae68a82f00a53a8d0164777d76
TrueZeorOne <blaabjerg.anton@gmail.com>
2023-04-20 08:55:42 +0200

    hotfix to exit safemode

--- HexIntersteller/Assets/Scripts/Combat/ShipSpawn.cs
+++ HexIntersteller/Assets/Scripts/Combat/ShipSpawn.cs
@@ -13,15 +13,18 @@ namespace HexInterstellar
         // Start is called before the first frame update
         void Start()
         {
-
+            OpenMenu.action.Enable();
         }
 
         // Update is called once per frame
         void Update()
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Physics.Raycast(ray, out raycast, 1000, gameObject.layer;
-            if(raycast.transform.gameObject == gameObject && )
+            Physics.Raycast(ray, out raycast, 1000, gameObject.layer);
+            if(raycast.transform.gameObject == gameObject && OpenMenu.action.ReadValue<float>() > 0)
+            {
+
+            }
 
         }
     }
